f919fa5282a58aa95474bfbd1565fc983395bd52
Zach <zsjostrom35@gmail.com>
2014-02-08 11:48:40 -0500

    Testing JDBC connection

---
+++ project/src/main/java/CSE5242/project/TestConnection.java
@@ -0,0 +1,39 @@
+package CSE5242.project;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Hello world!
+ *
+ */
+public class TestConnection {
+	
+    public static void main( String[] args ) {
+	    String dbUrl = "jdbc:mysql://127.0.0.1:3306/band";
+	    String dbClass = "com.mysql.jdbc.Driver";
+	    String query = "Select distinct(table_name) from INFORMATION_SCHEMA.TABLES";
+	    String username = "cse5242";
+	    String password = "cse5242";
+	    try {
+	
+	        Class.forName(dbClass);
+	        Connection connection = DriverManager.getConnection(dbUrl,
+	            username, password);
+	        Statement statement = connection.createStatement();
+	        ResultSet resultSet = statement.executeQuery(query);
+	        while (resultSet.next()) {
+	        String tableName = resultSet.getString(1);
+	        System.out.println("Table name : " + tableName);
+	        }
+	        connection.close();
+	    } catch (ClassNotFoundException e) {
+	        e.printStackTrace();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+    }
+}
