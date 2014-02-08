394c1870d04c065f25148e9574490a65dda2c3c4
Zach <zsjostrom35@gmail.com>
2014-02-08 11:20:04 -0500

    Initial commit

---
+++ project/src/main/java/CSE5242/project/App.java
@@ -0,0 +1,13 @@
+package CSE5242.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
